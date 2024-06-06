cc51ecb170df83e1fce9170dd28737fd73632852
lenakroeker <lenakroeker@gmail.com>
2024-06-06 02:37:32 -0400

    edit express path dist

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.get("/api/hello", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
 });
 
 dotenv.config();
