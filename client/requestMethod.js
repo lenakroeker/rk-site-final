e1f20d751c64c8fc21dee7462315e8a17e8d15c1
lenakroeker <lenakroeker@gmail.com>
2024-06-06 22:40:21 -0400

    update request methos url

--- client/requestMethod.js
+++ client/requestMethod.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL;
+import.meta.env.VITE_API_URL;
 
 export const publicRequest = axios.create({
-  baseURL: API_URL,
+  baseURL: VITE_API_URL,
 });
